d4730fe0e4dd912848b5e6990d35ff8f5b200858
Emily <kakon002@cougars.csusm.edu>
2018-09-24 11:27:11 -0700

    First Commit

---
+++ Assets/Scripts/menu2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu2 : MonoBehaviour {
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 20, 100, 50), "Menu"))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+
+
+}
